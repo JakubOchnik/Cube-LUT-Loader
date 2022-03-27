2cf2ca9e11aa144d1d4d49444c3c75807aaf7242
JakubOchnik <jakub.ochnik1@gmail.com>
2022-03-27 18:00:32 +0200

    Add Eigen Tensor support to nearest-val CUDA kernel

--- src/GPUImageProcess/LUT3D/applyNearestValueGpu.cu
+++ src/GPUImageProcess/LUT3D/applyNearestValueGpu.cu
@@ -1,22 +1,29 @@
 #include <GPUImageProcess/LUT3D/applyNearestValueGpu.cuh>
 
-namespace NearestValGpu {
-	void run(dim3 grid, unsigned char* image, char channels, float* LUT, char LUTsize, float opacity)
+namespace GpuNearestValDevice {
+	void run(dim3 threads, dim3 blocks, unsigned char* image, const char channels, float* LUT, const char LUTsize, const float opacity, const std::tuple<int, int>& imgSize)
 	{
-		applyNearestKernel << <grid, 1 >> > (image, channels, LUT, LUTsize, opacity);
+		applyNearestKernel <<<blocks, threads>>> (image, channels, LUT, LUTsize, opacity, std::get<0>(imgSize), std::get<1>(imgSize));
 		cudaDeviceSynchronize();
 	}
 }
 
 
-__global__ void applyNearestKernel(unsigned char* image, char channels, float* LUT, char LUTsize, float opacity)
+__global__ void applyNearestKernel(unsigned char* image, char channels, float* LUT, char LUTsize, float opacity, const int xMax, const int yMax)
 {
-	// get the number of pixel in flattened image array
-	const int x = blockIdx.x;
-	const int y = blockIdx.y;
-	const int pixel_index = (x + y * gridDim.x) * channels;
+	using uchar = unsigned char;
+	// Get the index of pixel in flattened image array
+	const int x = blockIdx.x * blockDim.x + threadIdx.x;
+	const int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-	// get original colors
+	if(x >= xMax || y >= yMax)
+	{
+		// Check if the kernel coordinates are out of image bounds
+		return;
+	}
+
+	const int pixel_index = (x + y * (gridDim.x * blockDim.x)) * channels;
+	// Get original colors of the processed pixel
 	const int b = image[pixel_index];
 	const int g = image[pixel_index + 1];
 	const int r = image[pixel_index + 2];
@@ -26,10 +33,13 @@ __global__ void applyNearestKernel(unsigned char* image, char channels, float* L
 	const unsigned int g_ind = roundf(g * (LUTsize - 1) / 255.0f);
 	const unsigned int r_ind = roundf(r * (LUTsize - 1) / 255.0f);
 
-	const int base_index = LUTsize * LUTsize * channels * r_ind + LUTsize * channels * g_ind + channels * b_ind;
+	// Full formula for getting the index in a flattened 4D (2x2x2x3) RowMajor tensor:
+	// idx = LUTsize * LUTsize * LUTsize * ch + LUTsize * LUTsize * b + LUTsize * g + r;
+	const int base_index = LUTsize * LUTsize * b_ind + LUTsize * g_ind + r_ind;
+	int chIncr{static_cast<int>(pow(LUTsize, 3))}; 
 
-	const unsigned char newB = LUT[base_index + 2] * 255;
-	const unsigned char newG = LUT[base_index + 1] * 255;
+	const unsigned char newB = LUT[base_index + 2 * chIncr] * 255;
+	const unsigned char newG = LUT[base_index + chIncr] * 255;
 	const unsigned char newR = LUT[base_index + 0] * 255;
 
 	unsigned char finalB = b + (newB - b) * opacity;
