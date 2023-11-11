6c14c28c8839edddd2e1ff284c9521fe459eddf9
JakubOchnik <jakub.ochnik1@gmail.com>
2023-11-11 14:31:32 +0100

    Free memory in TearDown in InputArgsParser tests

--- src/test/InputArgsParserTest.cpp
+++ src/test/InputArgsParserTest.cpp
@@ -1,84 +1,87 @@
 #include <gtest/gtest.h>
 #include <TaskDispatcher/TaskDispatcher.hpp>
+#include <string.h>
 
 using namespace ::testing;
 
-class InputArgsParserTest : public ::testing::Test {};
+class InputArgsParserTest : public ::testing::Test {
+protected:
+    char** arguments;
+    size_t argCount = 0;
 
-char** initialize2DArray(const std::vector<std::string>& args) {
-    char** arr = new char*[args.size() + 1];
-    for (int i{}; i < args.size(); ++i) {
-        const auto& argument = args[i];
-        arr[i] = new char[argument.size()];
-        std::strcpy(arr[i], argument.c_str());
+    char** initialize2DArray(const std::vector<std::string>& args) {
+        argCount = args.size() + 1;
+        arguments = new char*[argCount];
+        for (int i{}; i < argCount - 1; ++i) {
+            const auto& argument = args[i];
+            arguments[i] = new char[argument.size() + 1];
+            strncpy(arguments[i], argument.c_str(), argument.size() + 1);
+        }
+        // Final argument has to be nullptr
+        arguments[argCount - 1] = nullptr;
+        return arguments;
     }
-    arr[args.size()] = nullptr;
-    return arr;
-}
 
-void free2Darray(char** arr, unsigned int size) {
-    for (int i{}; i < size + 1; ++i) {
-        delete arr[i];
+    void free2Darray() {
+        if (argCount <= 0) {
+            return;
+        }
+
+        for (int i{}; i < argCount - 1; ++i) {
+            delete arguments[i];
+        }
+        delete arguments;
     }
-    delete arr;
-}
+
+    void TearDown() override {
+        free2Darray();
+    }
+};
 
 TEST_F(InputArgsParserTest, testHelp)
 {
     const std::vector<std::string> sourceArgs{"program", "-h"};
     const auto arguments = initialize2DArray(sourceArgs);
-    const auto argCount = sourceArgs.size();
 
-    TaskDispatcher dispatcher(argCount, arguments);
+    TaskDispatcher dispatcher(sourceArgs.size(), arguments);
     std::string helpText;
     const auto params = dispatcher.parseInputArgs(helpText);
     EXPECT_EQ(params.getShowHelp(), true);
     EXPECT_GT(helpText.size(), 0);
-
-    free2Darray(arguments, argCount);
 }
 
 TEST_F(InputArgsParserTest, emptyArgs)
 {
     const std::vector<std::string> sourceArgs{"program"};
     const auto arguments = initialize2DArray(sourceArgs);
-    const auto argCount = sourceArgs.size();
 
-    TaskDispatcher dispatcher(argCount, arguments);
+    TaskDispatcher dispatcher(sourceArgs.size(), arguments);
     std::string helpText;
     EXPECT_THROW(dispatcher.parseInputArgs(helpText), boost::program_options::error);
-
-    free2Darray(arguments, argCount);
 }
 
 TEST_F(InputArgsParserTest, multipleInterpolationMethods)
 {
     const std::vector<std::string> sourceArgs{"program", "-i", "abc.png", "-l", "test.cube", "-t", "-n"};
     const auto arguments = initialize2DArray(sourceArgs);
-    const auto argCount = sourceArgs.size();
 
-    TaskDispatcher dispatcher(argCount, arguments);
+    TaskDispatcher dispatcher(sourceArgs.size(), arguments);
     std::string helpText;
     const auto params = dispatcher.parseInputArgs(helpText);
     EXPECT_EQ(params.getInterpolationMethod(), InterpolationMethod::Trilinear);
-
-    free2Darray(arguments, argCount);
 }
 
-struct IncorrectInputTest : public ::testing::TestWithParam<std::vector<std::string>> {};
+struct IncorrectInputTest : public InputArgsParserTest, public ::testing::WithParamInterface<std::vector<std::string>> {};
 
 TEST_P(IncorrectInputTest, incorrectParam) {
     std::vector<std::string> sourceArgs{"program"};
     const auto& testArgs = GetParam();
     sourceArgs.insert(sourceArgs.end(), testArgs.begin(), testArgs.end());
     const auto arguments = initialize2DArray(sourceArgs);
-    const auto argCount = sourceArgs.size();
 
-    TaskDispatcher dispatcher(argCount, arguments);
+    TaskDispatcher dispatcher(sourceArgs.size(), arguments);
     std::string helpText;
     EXPECT_THROW(dispatcher.parseInputArgs(helpText), boost::program_options::error);
-
-    free2Darray(arguments, argCount);
 }
 
 INSTANTIATE_TEST_SUITE_P(
