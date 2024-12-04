afcd07fc06228b5bfb986392e7a3075fac35c159
JakubOchnik <jakub.ochnik1@gmail.com>
2024-12-04 21:20:00 +0100

    GH-66 Fix generating project with Conan/CMake/MSVC and CUDA

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,10 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps
+from conan.tools.microsoft import is_msvc
 
 class CubeLUTLoader(ConanFile):
     name = "Cube LUT Loader"
     requires = "taywee-args/6.4.6", "opencv/4.9.0", "eigen/3.4.0", "fmt/10.2.1"
-    generators = "CMakeDeps", "CMakeToolchain"
     settings = "os", "build_type", "compiler", "arch"
     build_policy = "missing"
     options = {
@@ -20,3 +20,12 @@ class CubeLUTLoader(ConanFile):
     def configure(self):
         if "shared" in self.options:
             self.options["opencv/*"].shared = self.options.shared
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+
+        tc = CMakeToolchain(self)
+        if is_msvc(self) and "vs_runtime" in tc.blocks.keys():
+            tc.blocks.remove("vs_runtime")
+        tc.generate()
